d10d98a047a934991f66ae5bc8e4761a7ffd1766
Kasia Rachuta <korachuta@gmail.com>
2016-07-11 00:01:05 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,25 +1,10 @@
+> # Reading the file with data
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+# Subsetting the dataset
+subdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-R version 3.2.4 (2016-03-10) -- "Very Secure Dishes"
-Copyright (C) 2016 The R Foundation for Statistical Computing
-Platform: x86_64-apple-darwin13.4.0 (64-bit)
-
-R is free software and comes with ABSOLUTELY NO WARRANTY.
-You are welcome to redistribute it under certain conditions.
-Type 'license()' or 'licence()' for distribution details.
-
-  Natural language support but running in an English locale
-
-R is a collaborative project with many contributors.
-Type 'contributors()' for more information and
-'citation()' on how to cite R or R packages in publications.
-
-Type 'demo()' for some demos, 'help()' for on-line help, or
-'help.start()' for an HTML browser interface to help.
-Type 'q()' to quit R.
-
-[R.app GUI 1.67 (7152) x86_64-apple-darwin13.4.0]
-
-[Workspace restored from /Users/katarzynarachuta/.RData]
-[History restored from /Users/katarzynarachuta/.Rapp.history]
-
-> # Reading the file with datadataFile <- "./data/household_power_consumption.txt"data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")# Subsetting the datasetsubdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]globalActivePower <- as.numeric(subdata$Global_active_power)png("plot1.png", width=480, height=480)hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")dev.off()
+globalActivePower <- as.numeric(subdata$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
