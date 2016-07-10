46478bae0f17bd0c6aa70d70b52c3ef20eaa5cea
Kasia Rachuta <korachuta@gmail.com>
2016-07-11 00:04:40 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -22,4 +22,15 @@ Type 'q()' to quit R.
 [Workspace restored from /Users/katarzynarachuta/.RData]
 [History restored from /Users/katarzynarachuta/.Rapp.history]
 
-> # Reading the file with datadataFile <- "./data/household_power_consumption.txt"data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")# Subsetting the datasetsubdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]#str(subSetData)datetime <- strptime(paste(subdata$Date, subdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") GlobalActivePower <- as.numeric(subdata$Global_active_power)png("plot2.png", width=480, height=480)plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")dev.off()
+> # Reading the file with data
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+# Subsetting the dataset
+subdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subdata$Date, subdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+GlobalActivePower <- as.numeric(subdata$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, GlobalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
